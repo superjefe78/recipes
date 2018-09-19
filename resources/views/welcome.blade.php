4c086a6d17936afc93b07b69d53d5755f5fc328a
Pedro Morales <superjefe78@yahoo.com>
2018-09-18 19:34:11 -0500

    changed name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Recipes
+                    Mel's Recipes
                 </div>
 
                 <div class="links">
