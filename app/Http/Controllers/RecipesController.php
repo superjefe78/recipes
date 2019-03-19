cf6774a9e0a36d04676de3782b45c16b2fb04e5e
Pedro Morales <superjefe78@yahoo.com>
2019-03-18 22:55:32 -0500

    added code to recipes

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -7,4 +7,41 @@ use Illuminate\Http\Request;
 class RecipesController extends Controller
 {
     //
+    public function names()
+    {
+        $var = Recipe::all()->pluck('name');
+        return View('Recipes');
+        // return Recipe::all()->pluck('name');
+    }
+ 
+    public function index()
+    {
+        return Recipe::all();
+    }
+ 
+    public function show($id)
+    {
+        return Recipe::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        return Recipe::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $article = Recipe::findOrFail($id);
+        $article->update($request->all());
+
+        return $article;
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $article = Recipe::findOrFail($id);
+        $article->delete();
+
+        return 204;
+    }
 }
